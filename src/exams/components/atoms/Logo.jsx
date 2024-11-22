78ac7bd93c11acea5fed4762c4afd22a87f3bf5c
MEHEDI HASAN <mehedialhasan1999@gmail.com>
2024-11-22 15:16:59 +0600

    add home page url in logo click, add home page button for mobile device

--- src/exams/components/atoms/Logo.jsx
+++ src/exams/components/atoms/Logo.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 
 const Logo = () => {
+    const homePageUrl = import.meta.env.VITE_HOME_PAGE_URL;
+
     return (
         <div className="">
-            <Link to="/" className="text-3xl font-semibold flex gap-2">
+            <Link to={homePageUrl} className="text-3xl font-semibold flex gap-2">
                 <img src="/logo/icon.jpeg" alt="logo" className="h-8 w-20 rounded-xl" />
             </Link>
         </div>
-    )
-}
+    );
+};
 
 export default Logo;
